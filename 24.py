07b0fb3486e3cae908ed129448a4479e3154034a
Aiyeohd <aiyeohd.sc@gmail.com>
2026-02-20 09:52:09 +0300

    Update 24.py

--- 24.py
+++ 24.py
@@ -1,7 +1,23 @@
 # 24_20968 
+# s = open('24_20968.txt').readline().strip().replace('*', '+')
+# j = res = n_st = 0
+# for i in range(len(s)):
+#     if s[i] == '+':
+#         if i == j or s[i-1] == '+' or s[i-1] in '13579':
+#             j = i + 1
+#         n_st = i + 1
+#     else:
+#         if i > n_st and s[n_st] == '0':
+#             j = i
+#             n_st = i
+#         if s[i] in '02468':
+#             res = max(res, i-j+1)
+# print(res)
+
+
 s = open('24_20968.txt').readline().strip().replace('*', '+')
-j = res = n_st = 0
-for i in range(len(s)):
+i = j = res = n_st = 0
+while i < len(s):
     if s[i] == '+':
         if i == j or s[i-1] == '+' or s[i-1] in '13579':
             j = i + 1
@@ -12,5 +28,5 @@ for i in range(len(s)):
             n_st = i
         if s[i] in '02468':
             res = max(res, i-j+1)
-
+    i += 1
 print(res)
