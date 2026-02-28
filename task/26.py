2911e64bed3b609c25dbf14d1c0532db448a7e3b
Aiyeohd <aiyeohd.sc@gmail.com>
2026-02-28 23:53:17 +0700

    Refactor old and upload new

--- task/26.py
+++ task/26.py
@@ -1,40 +1,89 @@
 # 25363 
-# with open("26_25363.txt", "r") as f:
+# num = 0
+# data = []
+# with open('26_25363.txt', 'r') as f:
 #     n = int(f.readline())
-#     j = 1
-#     data_1 = []
 #     for i in f.readlines():
-#         data = i.split()
-#         data_1.append((int(data[0]), 1, j))
-#         data_1.append((int(data[1]), 2, j))
-#         j += 1
+#         el1,el2 = i.split()
+#         data.append((num, int(el1), 1))
+#         data.append((num, int(el2), 2))
+#         num += 1
 
-# data_1.sort(key=lambda x:x[0])
-
+# set_answ = set()
 # answ = [0]*n
-
+# data.sort(key = lambda x:x[1])
 # start = 0
 # end = n-1
-# set_ = set()
-# last_el = 0
 
-# for i in data_1:
-#     if i[2] not in set_:
-#         set_.add(i[2])
-#         if i[1] == 1:
-#             answ[start] = i[2]
+# for i in data:
+#     if i[0] not in set_answ:
+#         if i[2] == 1:
+#             answ[start] = i[0]
 #             start += 1
 #         else:
-#             answ[end] = i[2]
+#             answ[end] = i[0]
 #             end -= 1
-#         last_el = i[2]
+#         set_answ.add(i[0])
+#         last_n = i[0]
 
-# for i in range(len(answ)):
-#     if answ[i] == last_el:
-#         print(last_el)
-#         print(n-i-1)
+# top_k = 0
+
+# for i in answ:
+#     if i == last_n:
+#         print(last_n + 1)
+#         print(n - top_k - 1)
+#         break
+#     top_k += 1
 
 
 # -----------------------------------------------------------------------------------
 
 
+home = []
+with open('26_24897.txt', 'r') as f:
+    n = int(f.readline())
+    for i in f.readlines():
+        el1,el2,el3 = i.split()
+        home.append((int(el2), int(el3), int(el1)))
+
+home.sort()
+
+max_seq = 0
+seq = 0
+min_num_req = float('inf')
+answ_home = 0
+num_p_m = 0
+num_p = home[0][1]
+num_req = home[0][2]
+
+for i in range(n-1):
+    if home[i][0] == home[i+1][0] and home[i][1]+1 == home[i+1][1]:
+        seq += 1
+    elif home[i][0] == home[i+1][0] and home[i][1] == home[i+1][1]:
+        continue
+    else:
+        if seq > max_seq:
+            max_seq = seq
+            answ_home = home[i][0]
+            min_num_req = num_req
+            num_p_m = num_p
+        if seq == max_seq:
+            if num_req < min_num_req:
+                answ_home = home[i][0]
+                min_num_req = num_req
+                num_p_m = num_p
+        
+        num_p = home[i+1][1]
+        num_req = home[i+1][2]
+        seq = 0
+
+if seq > max_seq:
+    max_seq = seq
+    answ_home = home[i-1][0]
+    num_p_m = num_p
+elif seq == max_seq:
+    if num_req < answ_req:
+        answ_home = home[i-1][0]
+        num_p_m = num_p
+
+print(answ_home, num_p_m)
